2cd63c8495b41063f0829f40eba2fcd97aea781a
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-06-17 11:28:47 -0300

    workout constructor

--- lib/blocs/workout_cubit.dart
+++ lib/blocs/workout_cubit.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../states/workout_state.dart';
 
 class WorkoutCubit extends Cubit<WorkoutState> {
-  WorkoutCubit(super.initialState);
-
+  WorkoutCubit() : super(const WorkoutInitial());
   
 }
