384b6f21b6ebb22473552f488f292a2ef314bd88
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-06-30 08:36:52 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,34 +14,33 @@ class WorkoutTime extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Workouts',
-      theme: ThemeData(
-          useMaterial3: true,
-          primarySwatch: Colors.indigo,
-          textTheme: const TextTheme(
-              bodyMedium: TextStyle(color: Color.fromARGB(255, 66, 74, 96)))),
-      home: MultiBlocProvider(
-          providers: [
-            BlocProvider<WorkoutsCubit>(create: (context) {
-              WorkoutsCubit workoutsCubit = WorkoutsCubit();
-              if (workoutsCubit.state.isEmpty) {
-                workoutsCubit.getWorkouts();
+        debugShowCheckedModeBanner: false,
+        title: 'My Workouts',
+        theme: ThemeData(
+            useMaterial3: true,
+            primarySwatch: Colors.indigo,
+            textTheme: const TextTheme(
+                bodyMedium: TextStyle(color: Color.fromARGB(255, 66, 74, 96)))),
+        home: MultiBlocProvider(
+            providers: [
+              BlocProvider<WorkoutsCubit>(create: (context) {
+                WorkoutsCubit workoutsCubit = WorkoutsCubit();
+                if (workoutsCubit.state.isEmpty) {
+                  workoutsCubit.getWorkouts();
+                }
+                return workoutsCubit;
+              }),
+              BlocProvider<WorkoutCubit>(create: (context) => WorkoutCubit())
+            ],
+            child: BlocBuilder<WorkoutCubit, WorkoutState>(
+                builder: (context, state) {
+              if (state is WorkoutInitial) {
+                return const HomePage();
+              } else if (state is WorkoutEditing) {
+                return const EditWorkoutPage();
               }
-              return workoutsCubit;
-            }),
-            BlocProvider<WorkoutCubit>(create: (context) => WorkoutCubit())
-          ],
-          child: BlocBuilder<WorkoutCubit, WorkoutState>(
-              builder: (context, state) {
-            if (state is WorkoutInitial) {
-              return const HomePage();
-            } else if (state is WorkoutEditing) {
-              return const EditWorkoutPage();
-            }
 
-            return Container();
-          }))
-    );
+              return Container();
+            })));
   }
 }
